757a4f0d6e5d499de480bb488efc8fb4076e3d6f
koki93 <kokihisar@gmail.com>
2017-06-17 22:56:54 +0200

    main-navigation
    Izbrisano sve u klasi

--- header.php
+++ header.php
@@ -32,9 +32,8 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span>
 		      </button>
-		     	<a class="navbar-brand" href="home.php">GAMES<br>SERBIA</a>
+		     	<a class="navbar-brand" href="">GAMES<br>SERBIA</a>
 		    </div>
-
 		    <!-- Collect the nav links, forms, and other content for toggling -->
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right">
