9a763bf792687d3801e8020f36ef1b0a428cc2e2
Nikola Milosevic <nikolahissar@gmail.com>
2017-06-17 22:59:25 +0200

    jquery to bootstrap in function

--- functions.php
+++ functions.php
@@ -110,7 +110,7 @@ function esports_srbija_scripts() {
 	wp_enqueue_style( 'esports-srbija-style', get_stylesheet_uri() );
 
 	// Scripts
-	wp_enqueue_script( 'esports-srbija-navigation', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.7', true );
+	wp_enqueue_script( 'esports-srbija-navigation', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.7', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
